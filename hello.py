3cc1ffcbfd8af511fa37ac26994989ee25e9e8b0
student <student@SaigonTechs-iMac-2.local>
2021-01-29 17:10:05 +0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# for i in range (100):
+#     print("hello")
+
+# for i in [1, 2, 3, 4]:
+#     print(i)
+
+# for s in ["Alice", "John", "bob"]:
+#     print(len(s)) #do dai cua chu trong 1 tu
+
+for i in range (1, 5):
+    print(i)
---
+++ main.py
@@ -0,0 +1,13 @@
+s1 = input("Input value 1 ")
+s2 = input("Input value 2 ")
+s3 = input("Input value 3 ")
+# d = max(s1,s2)
+# if float(s1) > float(s2):
+#     d = s1
+# else:
+#     d = s2
+# if d > s3:
+#     print("The maximum value is" + d)
+# else:
+#     print("The maximum value is " + s3)
+print(max(s1, s2, s3))
